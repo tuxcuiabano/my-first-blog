53fa0abfda4a9b4adbb18bad13d5e1e0170bc4de
pedro <pedro@tuxcuiabano.com>
2023-01-31 00:51:12 -0400

    Correcao CSS 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,3 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
